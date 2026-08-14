19aa21737b2e7fa534ca710eb32477627ffcaf29
emmanuel-le-dev <emmanuelnayoboss@gmail.com>
2026-08-14 14:42:47 -0200

    chore: initialize Spring Boot library API

---
+++ src/test/java/com/ghost/library/DigitalLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghost.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
